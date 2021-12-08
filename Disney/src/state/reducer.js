829408222a15988dba7e10f3acc9ab27ed761134
Jessweb87 <jprosper87@gmail.com>
2021-12-08 17:46:44 -0500

    added h1 in App.js it's starting to work

---
+++ Disney/src/state/reducer.js
@@ -0,0 +1,12 @@
+const initialState = {
+    loading: false,
+    error: "",
+    success: {}
+};
+
+const reducer = ( state = initialState, action) => {
+    switch (action.type) {
+        default:
+            return state
+    }
+}
